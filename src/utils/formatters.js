7310866cdbea3398e1065f42f5b1e8df06fae0a5
Carson Batchelor <carsonb1723@gmail.com>
2026-02-25 18:52:10 -0600

    Small decimal fix

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -2,7 +2,7 @@ export function formatCurrency(amount) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-    minimumFractionDigits: 0,
+    minimumFractionDigits: 2,
     maximumFractionDigits: 2,
   }).format(amount ?? 0)
 }
